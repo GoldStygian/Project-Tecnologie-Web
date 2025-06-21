01f2a93a3ec47d9f8f512e79bbb67d9c4b6a2915
Raffaele <92647402+GoldStygian@users.noreply.github.com>
2025-06-21 18:27:07 +0200

    increase layoute response

--- Frontend/streetcats/src/app/navbar/navbar.ts
+++ Frontend/streetcats/src/app/navbar/navbar.ts
@@ -2,16 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { AuthService } from '../_services/auth-service/auth-service';
+import { ThemeSwitch } from '../_services/theme-switch/theme-switch';
+import { Sidenav } from '../sidenav/sidenav';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterModule, RouterLink, RouterLinkActive, Sidenav],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.scss']
 })
 export class Navbar {
 
   authService = inject(AuthService);
+  themeService = inject(ThemeSwitch);
+
+  sidenavOpen = false;
+
+  toggleSidenav() {
+    this.sidenavOpen = !this.sidenavOpen;
+  }
+
+  closeSidenav() {
+    this.sidenavOpen = false;
+  }
 
 }
--- Frontend/streetcats/src/app/sidenav/sidenav.ts
+++ Frontend/streetcats/src/app/sidenav/sidenav.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { AuthService } from '../_services/auth-service/auth-service';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss'
 })
 export class Sidenav {
 
+  authService = inject(AuthService);
+  @Input() isOpen = false;
+
+  close() {
+    this.isOpen = false;
+  }
 }
